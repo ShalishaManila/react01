f3f6e8b73e8eab71edc5292f5525475cd54b14ba
ShalishaManila <shalisha.manila@gmail.com>
2025-04-22 20:53:18 +0200

    Cerated basic structure

---
+++ shali-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Projects from './Components/Projects.jsx';
+
+function App() {
+  
+  return (
+    <>
+      <Projects title="My Title" />
+    </>
+  )
+}
+
+export default App
---
+++ shali-react-app/src/Components/Projects.jsx
@@ -0,0 +1,9 @@
+function Projects(props) {
+  return (
+    <>
+      <h1>{props.title}</h1>
+    </>
+  );
+}
+
+export default Projects;
