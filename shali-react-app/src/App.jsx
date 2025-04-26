3f66b88a8806e3a779ab8d359f36d76a59dadc31
ShalishaManila <shalisha.manila@gmail.com>
2025-04-26 22:44:44 +0200

    Created components

--- shali-react-app/src/App.jsx
+++ shali-react-app/src/App.jsx
@@ -1,12 +1,96 @@
-import Projects from './Components/Projects.jsx';
+import { useState } from "react";
+
+import svgproject from './assets/images/svg-project.jpg';
+import bootstrapproject from './assets/images/bootstrap-project.jpg';
+import groupproject from './assets/images/group-project.jpg';
+
+import "./App.css";
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 
 function App() {
-  
+  const [count, setCount] = useState(0);
+
   return (
     <>
-      <Projects title="My Title" />
+      <div className="container">
+        <Card >
+          <Card.Img variant="top" src={svgproject} />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+          </ListGroup>
+          <Card.Body>
+            <Card.Link href="#">Card Link</Card.Link>
+            <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>
+
+        <Card >
+          <Card.Img variant="top" src={bootstrapproject} />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+          </ListGroup>
+          <Card.Body>
+            <Card.Link href="#">Card Link</Card.Link>
+            <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>
+
+        <Card >
+          <Card.Img variant="top" src={groupproject} />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+          </ListGroup>
+          <Card.Body>
+            <Card.Link href="#">Card Link</Card.Link>
+            <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>
+      </div>
+      <div className="card text-center">
+        <div className="overflow">
+          <img className="card-img-top" src={svgproject} alt="SVG Project Screenshot" />
+        </div>
+        <div className="card-body text-dark">
+          <h4 className="card-title">SVG Project</h4>
+          <p className="card-text text-secondary">
+            asdfa asdfasdf
+          </p>
+          <a href="#" className="btn btn-outline-success">Link to Github</a>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- shali-react-app/src/Components/Projects.jsx
+++ shali-react-app/src/Components/Projects.jsx
@@ -1,9 +1,50 @@
-function Projects(props) {
+import { useState } from "react";
+
+import svgproject from "../assets/images/svg-project.jpg";
+import bootstrapproject from "../assets/images/bootstrap-project.jpg";
+import groupproject from "../assets/images/group-project.jpg";
+
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Project from "./Project.jsx";
+
+function Projects() {
+  const myProjects = [
+    {
+      name: "SVG",
+      techs: ["css", "html", "graphics"],
+      screenshot: svgproject,
+      about:
+        "This project helped to understand graphics and it was challenging initially to disign some of the basic shapes.",
+      giturl: "https://github.com/ShalishaManila/ITTFW6",
+    },
+    {
+      name: "Bootstrap",
+      techs: ["css", "html", "js", "graphics"],
+      screenshot: bootstrapproject,
+      about:
+        "It was fun learning Bootstrap as it simplified css coding. It was fun creating navbar, buttons and cards with ease and also responsive.",
+      giturl: "https://github.com/ShalishaManila/ITTFW11",
+    },
+    {
+      name: "Group Project",
+      techs: ["css", "html", "graphics"],
+      screenshot: groupproject,
+      about:
+        "This was my frist group project. My role was to create Gallery for the Spa website. Finding the right layout, animations and images was challenging to learn.",
+      giturl: "https://github.com/berriestrawberrie/Group-Project-Spa-Website",
+    },
+  ];
   return (
-    <>
-      <h1>{props.title}</h1>
-    </>
+    <div className="container-fluid d-flex justify-content-center">
+      <div className="row">
+        {myProjects.map((myProject) => (
+          <div className="col-md-4">
+            <Project key={myProject.name} myProject={myProject} />
+          </div>
+        ))}
+        ;
+      </div>
+    </div>
   );
 }
-
 export default Projects;
